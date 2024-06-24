ff84098b7b39bbbd93f68c01ec8ed7c3d68bb88b
LewisMK <79156165+LewisMK@users.noreply.github.com>
2024-06-24 11:36:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,27 @@
+function skillsMember() {
+  return {
+    name: 'member',
+    type: 'skills',
+    data: {
+      name: 'Member',
+      description: 'Member of the team',
+      skills: [
+        {
+          name: 'Skill 1',
+          description: 'Description',
+          value: 0
+        },
+        {
+          name: 'Skill 2',
+          description: 'Description',
+          value: 0
+        },
+        {
+          name: 'Skill 3',
+          description: 'Description',
+          value: 0
+        }
+      ]
+    }
+  };
+}
